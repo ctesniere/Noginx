ca548a160c2d8f5e59608c2b52d93e90f28ac33c
philippeyuen <philippeyuen@gmail.com>
2014-05-08 15:37:59 +0200

    Chat

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express')
+var app = express();
+var io = require('socket.io');
+
+io = io.listen(app.listen(8080));
+
+// routing
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+var usernames = {};
+
+io.sockets.on('connection', function (socket) {
+	socket.on('sendchat', function (data) {
+		io.sockets.emit('updatechat', socket.username, data);
+	});
+	socket.on('adduser', function(username){
+		socket.username = username;
+		usernames[username] = username;
+		socket.emit('updatechat', 'SERVER', 'Connected');
+		socket.broadcast.emit('updatechat', 'SERVER', username + ' has joined');
+		io.sockets.emit('updateusers', usernames);
+	});
+
+	socket.on('disconnect', function(){
+		delete usernames[socket.username];
+		io.sockets.emit('updateusers', usernames);
+		socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+	});
+});
