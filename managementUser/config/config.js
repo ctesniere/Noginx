cd84f00b200dabd4878156ae91e5f711e1c204af
Cédric TESNIERE <tesniere.cedric@gmail.com>
2014-05-08 22:57:47 +0200

    Add config

--- managementUser/config/config.js
+++ managementUser/config/config.js
@@ -1,10 +1,13 @@
 var Config = {
-    "mongo": {
+    mongo: {
         host:"127.0.0.1",
         port:"27017",
-        db:"nodetest"
+        db:"nodetest",
+        table : {
+            userlist:"userlist"
+        }
     },
-    "process_port": 3000
+    process_port: 3000
 };
 
 module.exports = Config;
